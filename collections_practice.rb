f0f97ddbd89176f8c54c760413fe217f74a553f4
cd <coraldarling@outlook.com>
2018-09-09 23:56:11 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,24 @@
-# your code goes here
+require 'pry'
+
+def begins_with_r(array)
+  yield.all? if block_given?
+  #binding.pry
+end
+
+begins_with_r do |word|
+  word.starts_with?("r")
+end
+ 
+
+#def contain_a(array)  |word|
+  #if word.include?("a") == true
+#end
+
+#def first_wa(array)
+  #array.detect
+#end
+
+
+#def organize_schools(schools)
+  
+#end
